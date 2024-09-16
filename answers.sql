2e5cd501a75692268ceefe4e569c55e001fdb34b
Eqush <eliasmwangi154@gmail.com>
2024-09-16 12:42:54 +0100

    Add files via upload

--- answers.sql
+++ answers.sql
@@ -1,63 +1,69 @@
--- CREATE DATABASE hospital_db
-
 -- USE hospital_db;
 
--- CREATE TABLE patients(
---   patient_id INT PRIMARY KEY AUTO_INCREMENT,
---    first_name VARCHAR(255) NOT NULL,
---  last_name VARCHAR(255) NOT NULL,
---    date_of_birth DATE NOT NULL,
---    gender VARCHAR(255),
---    language VARCHAR(255) NOT NULL
--- );
+-- Select only first name,last name and date of birth
+-- SELECT * FROM patients;
+-- SELECT first_name, last_name and date_of_birth
+-- FROM patients;
 
- -- CREATE TABLE patients(
- -- patient_id INT PRIMARY KEY AUTO_INCREMENT,
- -- first_name VARCHAR(50) NOT NULL,
- -- last_name VARCHAR(50) NOT NULL,
- -- date_of_birth DATE NOT NULL,
- -- gender VARCHAR(10),
- -- language VARCHAR(20) NOT NULL
--- );
+-- Select only providers id,first name and provider specialty
+-- SELECT * FROM providers;
+-- SELECT provider_id, first_name and provider_specialty
+-- FROM providers;
 
--- CREATE TABLE visits(
---  visit_id INT PRIMARY KEY AUTO_INCREMENT,
---  patient_id INT NOT NULL,
---  provider_id INT NOT NULL,
---  date_of_visit DATE NOT NULL,
---  date_scheduled DATE NOT NULL,
---  visit_department_id INT NOT NULL,
---  visit_type VARCHAR(255) NOT NULL,
---  blood_pressure_systollic INT,
---  blood_pressure_diastollic DECIMAL,
---  pulse DECIMAL,
---  visit_status VARCHAR(255) NOT NULL
--- );
+-- retrieving names of patients whose name starts with "Ab"
+-- SELECT * FROM patients
+-- WHERE first_name LIKE "Ab%";
 
--- CREATE TEABLE ed_visits(
---  ed_visit_id INT PRIMARY KEY AUTO_INCREMENT,
--- visit_id INT NOT NULL,
--- patient_id INT NOT NULL,
--- acuity INT NOT NULL,
--- reason_for_visit VARCHAR(255) NOT NULL,
--- disposition VARCHAR(255) NOT NULL
---  );
+-- retrieving providers whose specialties end with "y"
+-- SELECT * FROM providers
+-- WHERE provider_specialty LIKE "y%";
+
+-- retrieve all patients born after 01 January 1980
+-- SELECT * FROM patients
+-- WHERE date_of_birth > "1980-01-01";
+
+-- retrieving visits where acuity level is 2 or higher
+--  SELECT * FROM ed_visits
+ -- WHERE acuity > "2";
+
+-- retrieving the language spoken
+-- SELECT * FROM patients;
+-- SELECT language= ="spanish"
+-- FROM patients;
+
+-- retrieving visits with reason and disposition
+-- SELECT * FROM ed_visits
+-- WHERE reason_for_visit ="migraine"
+-- AND disposition = "admissions";
+
+-- retrieving birth date of patients born between 1975 nad 1980
+-- SELECT * FROM patients
+-- WHERE date_of_birth BETWEEN '1975-01-01' AND '1980-12-31';
+
+-- retrieving patients names and sorting them in alphabetical order
+-- SELECT first_name, last_name
+-- FROM patients
+-- ORDER BY first_name ASC;
+ 
+-- retrieving all visits starting from the most recent
+-- SELECT* FROM visits
+-- ORDER BY date_of_visit DESC;
+  
+-- retrieve primary diagnosis is "Stroke"  discharge disposition is "Home".
+-- SELECT *
+-- FROM admissions
+-- WHERE primary_diagnosis = "stroke"
+-- AND disharge_disposition = "Home";
+
+-- providers who joined after 1995 and specialize in Pediatrics or Cardiology
+-- SELECT *
+-- FROM providers
+-- WHERE date_joined > "1995-12-31"
+-- AND provider_specialty IN ("pediatrics, cardiology");
+   
 
 
---  CREATE TABLE admissions(
---  admission_id INT PRIMARY KEY AUTO_INCREMENT,
---  patient_id INT NOT NULL,
---  admission_date DATE NOT NULL,
---  discharge_date DATE NOT NULL,
---  disharge_disposition VARCHAR(255) NOT NULL,
---  service VARCHAR(255) NOT NULL,
---  primary_diagnosis VARCHAR(255) NOT NULL
---  );
-    
---  CREATE TABLE discharges(
---  discharge_id INT PRIMARY KEY AUTO_INCREMENT,
---  admission_id INT NOT NULL,
---  patient_id INT NOT NULL,
---  discharge_date DATE NOT NULL,
---  discharge_disposition VARCHAR(255) NOT NULL
---  );
+
+
+
+
